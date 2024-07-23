de923530984cd6d080b40f27724b8bb94aa93d55
AWKrol <92876406+AWKrol@users.noreply.github.com>
2024-07-23 23:01:10 +0300

    pipeline

---
+++ apiTests.groovy
@@ -0,0 +1,30 @@
+import myUtils.groovy
+
+timeout(60){
+    node("maven"){
+        prepareConfig()
+        def jobDescription = """
+            api tests
+         """
+        try {
+            currentBuild.description=jobDescription;
+            def testContainerName = "api_tests_$BUILD_NUMBER"
+            stage ("Run API Tests ${jobDescription}"){
+
+                sh "docker run --rm --network=host --name $testContainerName -t localhost:5005/apitests"
+            }
+//            stage("Publish allure report"){
+//                allure([
+//                        disabled:true,
+//                        results:["$pwd/allure-results"]
+//                ])
+//            }
+//            stage("Telegram notification"){
+//
+//            }
+        }
+        finally {
+            sh "docker stop $jobDescription"
+        }
+    }
+}
