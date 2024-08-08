a0468495bac6cbe328f63ec26fcc92f21c482842
AWKrol <92876406+AWKrol@users.noreply.github.com>
2024-08-08 23:47:21 +0300

    pipeline

--- apiTests.groovy
+++ apiTests.groovy
@@ -109,3 +109,9 @@ pipeline {
 def triggerJob(def jobName) {
     build job: "$jobName"
 }
+
+def prepareConfig(){
+    def yamlConfig = readYaml text: $YAML_CONFIG
+
+    yamlConfig.each(k, v -> System.setProperty(v))
+}
