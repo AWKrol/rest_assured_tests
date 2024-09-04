691d394d4499bcb2cc60cb3cde46cad1bea866da
AWKrol <92876406+AWKrol@users.noreply.github.com>
2024-09-04 21:41:32 +0300

    pipeline

--- apiTests.groovy
+++ apiTests.groovy
@@ -7,19 +7,19 @@ pipeline {
 //        CHAT_ID = credentials('chatID')
 //    }
     stages {
-//        stage('Prepare Environment') {
-//            steps {
-//                script {
-//                    echo "Preparing environment..."
-//                    env.TESTS_TYPE = params.TESTS_TYPE ?: null
-//                    echo "Configuration: TESTS_TYPE=${env.TESTS_TYPE}"
-//
-//                    if (!env.TESTS_TYPE) {
-//                        error("TESTS_TYPE is not defined")
-//                    }
-//                }
-//            }
-//        }
+        stage('Prepare Environment') {
+            steps {
+                script {
+                    echo "Preparing environment..."
+                    env.TESTS_TYPE = params.TESTS_TYPE ?: null
+                    echo "Configuration: TESTS_TYPE=${env.TESTS_TYPE}"
+
+                    if (!env.TESTS_TYPE) {
+                        error("TESTS_TYPE is not defined")
+                    }
+                }
+            }
+        }
         stage('Checkout') {
             steps {
                 script {
